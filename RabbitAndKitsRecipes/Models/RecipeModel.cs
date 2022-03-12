3d5126194eafc767ee39e29b0e8dc55d042fef9a
CyborgChameleon <62155152+CyborgChameleon@users.noreply.github.com>
2022-03-12 16:43:21 -0500

    Add files via upload

---
+++ RabbitAndKitsRecipes/Models/RecipeModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RabbitAndKitsRecipes.Models
+{
+    public class RecipeModel
+    {
+        public string recipeName { get; set; }
+
+
+        //this is clean version
+        //things to add
+        //    chef
+        //    description
+        //    ingredients
+        //    instructions
+
+    }
+}
