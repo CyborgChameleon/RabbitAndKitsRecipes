79df42ec35d3e74ad709b125975f1466b7ac5ac3
CyborgChameleon <diabloftdih@gmail.com>
2022-03-12 22:31:20 -0500

    add description to Recipes.cs

--- RabbitAndKitsRecipes/Models/Recipes.cs
+++ RabbitAndKitsRecipes/Models/Recipes.cs
@@ -16,6 +16,28 @@ namespace RabbitAndKitsRecipes.Models
 
         public string chef { get; set; }
 
+        public string description { get; set; }
+
+        //add the following
+        //  -description
+        //  -ingredient
+        //  -instructions
+        //  -instructions with photo
+
+        //in the following places
+        //  -Recipes.cs
+        //  -HomeController.cs??? not sure if necessary-> if not delete that part
+        //  -
+
+
+        //TESTING ZONE
+        //A.Add controler with recipe model (install package if needed)
+        //B.add database via package manager console
+        //  1.enable-migrations
+        //  2.add-migration "initialsetup"
+        //  3.update-database
+        //
+
 
     }
 }
