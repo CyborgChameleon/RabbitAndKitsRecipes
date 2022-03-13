cbce9cbee482f2737b40f63e8a07e17727ad08fd
CyborgChameleon <diabloftdih@gmail.com>
2022-03-12 21:37:31 -0500

    a

---
+++ RabbitAndKitsRecipes/Models/Recipes.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RabbitAndKitsRecipes.Models
+{
+    public class Recipes
+    {
+        [Key]
+        public int recipeID { get; set; }
+
+        [Required]
+        public string recipeName { get; set; }
+
+        public string chef { get; set; }
+
+
+    }
+}
